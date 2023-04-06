12df6b8d60687cb889a495469a128e0007349091
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2023-04-05 21:17:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            int n2;
+            int soma;
+            Console.Write("Digite o 1º número: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Digite o 2º número: ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            soma = n1 + n2;
+            Console.Write("Resultado da soma: ");
+            Console.WriteLine(soma);
+            Console.WriteLine();
+        }
+    }
+}
